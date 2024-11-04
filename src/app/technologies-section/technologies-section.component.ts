35a980c8fd7bda58032d6168186eb9e791b9097a
Eugen1188 <eugen.ferchow@gmx.de>
2024-11-04 03:53:32 +0100

    first commit

--- src/app/technologies-section/technologies-section.component.ts
+++ src/app/technologies-section/technologies-section.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-technologies-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './technologies-section.component.html',
   styleUrl: './technologies-section.component.scss'
 })
 export class TechnologiesSectionComponent {
+
+  skills: string[] = ["HTML", "CSS", "JavaScript", "Material Design", "Typescript", "Angular", "Firebase", "GIT", "Rest-Api", "Scrum", "Growth mindset"];
 
 }
