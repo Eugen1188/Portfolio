314f475160afbb45600cdc401e2640e7c379df63
Eugen1188 <eugen.ferchow@gmx.de>
2025-02-27 18:53:03 +0100

    update_profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from "./landing-page/landing-page.component";
-import { AboutMeSectionComponent } from "./about-me-section/about-me-section.component";
-import { TechnologiesSectionComponent } from "./technologies-section/technologies-section.component";
-import { PortfolioSectionComponent } from "./portfolio-section/portfolio-section.component";
-import { SliderComponent } from "./slider/slider.component";
-import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
-import { RouterLinkActive, RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { HeaderComponent } from "./landing-page/header/header.component";
 import { LanguageService } from './language.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule, FooterComponent, HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Portfolio';
-
-  hiddeBackground = inject(LanguageService);
+  title = 'DaBubble';
 }
