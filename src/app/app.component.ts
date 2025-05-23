189478739a3c2fb0d4a56a621d9d431bade8e90b
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-23 20:49:41 +0200

    change_city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./landing-page/header/header.component";
+import { RouterModule } from '@angular/router';
 import { LanguageService } from './language.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
